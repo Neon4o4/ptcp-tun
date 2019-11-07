775b63fc70200b9f3402cf5cf45a1aedfda59116
Neon4o4 <baiyinshuo@clatter.cn>
2019-11-08 01:29:35 +0800

    minor fix: seq in client; append accepted connection to polling; create new sockets

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import socket
 import select
 from collections import defaultdict
 
+from buffer import Steam2PacketBuffer
+
 listen_addr = ('0.0.0.0', 1234)
 remote_addr = ('127.0.0.1', 4321)
 target_addr = ('127.0.0.1', 1080)
 
 chunk_size = 1024 * 32
 listen_backlog = 256
-
+outgoing_incoming_rate = 2
 '''
 For client
 application  --incoming--> client --outgoing--> server
@@ -21,7 +23,7 @@ client --incoming--> server --outgoing--> target
 def client():
     incoming_socks = set()
     incoming_sending_caches = defaultdict(list)  # incoming socket -> [ (seq, bytes) ]
-    incoming_sending_seq = defaultdict(lambda: 0)  # incoming socket -> last send
+    incoming_sending_seq = defaultdict(lambda: 1)  # incoming socket -> last send
     outgoing_socks_map = {}  # outgoing socket -> incoming socket (n -> 1 mapping)
     read_socks, write_socks, exp_socks = [], [], []
     listen_sock = socket.socket()
@@ -35,18 +37,24 @@ def client():
                 # client new connection
                 incoming, _ = r_sock.accept()
                 incoming_socks.add(incoming)
+                for _ in range(outgoing_incoming_rate):
+                    outgoing = socket.socket()
+                    outgoing.connect(remote_addr)
+                    outgoing_socks_map[outgoing] = incoming
+                    read_socks.append(outgoing)
+                    write_socks.append(outgoing)
+                    exp_socks.append(outgoing)
+
+                read_socks.append(incoming)
+                write_socks.append(incoming)
+                exp_socks.append(incoming)
                 continue
             if r_sock in incoming_socks:
                 # client sending data
-                last_chunk = incoming_sending_caches.get(r_sock)
-                if not last_chunk:
-                    max_sending_seq = 1
-                else:
-                    max_sending_seq = last_chunk[-1] + 1
+                sending_seq = incoming_sending_seq[r_sock] + 1
                 data = r_sock.recv(chunk_size)
-                if last_chunk is None:
-                    incoming_sending_caches[r_sock] = []
-                incoming_sending_caches[r_sock].append((max_sending_seq, data))
+                incoming_sending_caches[r_sock].append((sending_seq, data))
+                incoming_sending_seq[r_sock] = sending_seq
             else:
                 # server replying data
                 in_sock = outgoing_socks_map.get(r_sock, None)
